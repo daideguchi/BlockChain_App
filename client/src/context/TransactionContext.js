740854aa7ec1ab16526e0f4630bb131c9a55ff9e
daideguchi <dd.1107.11107@gmail.com>
2022-08-18 12:56:13 +0900

    220818

--- client/src/context/TransactionContext.js
+++ client/src/context/TransactionContext.js
@@ -1,10 +1,12 @@
 import { ethers } from "ethers";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { contractABI, contractAddress } from "../utils/connect";
 
 //contextを作るときは、頭文字大文字で始めるのが通常
 //"react"にあるcontextを呼んでくる。そうするとcontextを作ることができる
 export const TransactionContext = createContext();
+
+const { ethereum } = window;
 
 //スマートコントラクトの取得 ※自動契約プログラム
 //window.ethereumはインストールしたメタマスクの情報を見ることができるもの
@@ -30,14 +32,49 @@ const getSmartContract = () => {
 //TransactionProviderの中に、ウォレット連携、通貨のやり取りなどをする関数を記述する
 //記述したプロバイダは、どのコンポーネントにも渡せるように記述する
 export const TransactionProvider = ({ children }) => {
-    
+
+  //※１アカウントのログイン情報を保存しておくためにuseStateを使う
+  const [currentAccount, setCurrentAccount] = useState("");
+
+    //メタマスクウォレットと連携しているか確認
+  const checkTransactionWalletConnected = async () => {
+    if (!ethereum) return alert("メタマスクをインストールしてください");
+
+    //メタマスクのアカウントIDを取得
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
+
+  };
+
+  //メタマスクウォレットと連携する
+  const connectWallet = async () => {
+    if (!ethereum) return alert("メタマスクをインストールしてください");
+
+    //メタマスクを持っていれば接続を開始する
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+    console.log(accounts[0]);
+
+    //上の※１にログインしているアドレスを渡す
+    setCurrentAccount(accounts[0]);
+  }
+
+  
+
+
+  //useEffectの使い方。第二引数に依存環境を示し、その変数が実行されたときだけ、
+  //指定の関数を読み込むようにする。今回は空なのでページリロード時一度だけ実行
+  //ログインしているか確認するもの。ログインすれば[]にアドレスが入る
+  useEffect(() => {
+    checkTransactionWalletConnected();
+  }, []);
 
   return (
     //ここで囲まれた{children}はTransactionContextで宣言された変数・関数をいつでも呼び出すことができる。
     //どんな変数を渡したいかは「value」で指定することができる。
     //下記のようにすることで、この{{ name: "dd1107" }}プロパティの情報は
     // childrenでいつでも使うことができる
-    <TransactionContext.Provider value={{ name: "dd1107" }}>
+    <TransactionContext.Provider value={{ connectWallet }}>
       {children}
     </TransactionContext.Provider>
   );
